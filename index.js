c26cd5524c2661495dccaa4955db1e5d5bbaf14d
arturovt <arthurandrosovich@gmail.com>
2026-03-12 23:25:46 +0200

    fix: ensure require is available in ESM environments via caller-provided polyfill
    [no ci]

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { load } = require('node-gyp-build-esm');
 
 const binding = load(__dirname, () => ({
-  'linux-x64': () => require('./prebuilds/linux-x64+ia32/procstat-napi.node'),
+  'linux-x64': () =>
+    require(/* @vite-ignore */ './prebuilds/linux-x64+ia32/procstat-napi.node'),
   'darwin-x64': () =>
-    require('./prebuilds/darwin-x64+arm64/procstat-napi.node'),
-  'win32-x64': () => require('./prebuilds/win32-x64+ia32/procstat-napi.node'),
+    require(
+      /* @vite-ignore */ './prebuilds/darwin-x64+arm64/procstat-napi.node',
+    ),
+  'win32-x64': () =>
+    require(/* @vite-ignore */ './prebuilds/win32-x64+ia32/procstat-napi.node'),
 }));
 
 module.exports = { xml2json: binding.xml2json };
